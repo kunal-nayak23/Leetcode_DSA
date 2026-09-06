73ecb7f84807e77c7aa2cedc45743e68e48c1c32
kunal-nayak23 <nayakkunal2006@gmail.com>
2026-09-06 19:49:02 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (76.10%) - LeetSync

---
+++ 86-partition-list/partition-list.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode before(0), after(0);
+        ListNode* b = &before;
+        ListNode* a = &after;
+
+        while(head){
+            if(head->val < x){
+                b->next = head;
+                b = b->next;
+            } else {
+                a->next = head;
+                a = a->next;
+            }
+            head = head->next;
+        }
+
+        a->next = NULL;
+        b->next = after.next;
+
+        return before.next;
+    }
+};
